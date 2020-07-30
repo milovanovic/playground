4f96c703f00f50b6217c89b634f0a7650a9358e1
baywatcher <>
2020-07-30 13:22:46 +0200

    Tweaks install iverilog script.

--- .install_iverilog.sh
+++ .install_iverilog.sh
@@ -2,6 +2,7 @@ set -e
 if [ ! -f $INSTALL_DIR/bin/iverilog ]; then
   mkdir -p $INSTALL_DIR
   git clone https://github.com/steveicarus/iverilog
+  cd iverilog
   sh autoconf.sh
   ./configure
   make
