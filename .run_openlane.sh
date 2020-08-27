f0362bee7de44e028b5bc4e56c13b295c983bbc5
baywatcher <>
2020-08-27 15:42:51 +0200

    Trying to setup Travis with PDKs and Openlane.

--- .run_openlane.sh
+++ .run_openlane.sh
@@ -4,7 +4,7 @@ set -ev
 # Setup docker container
 #docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2
 #docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 "flow.tcl -design spm"
-docker exec -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "./flow.tcl -design spm"
+docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "./flow.tcl -design spm"
 # Run flow for design SPM as test
 #source flow.tcl -design spm
 #exit
