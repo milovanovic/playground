a72f446da188893aed5857b1c65fdda1658f0748
baywatcher <>
2020-08-27 09:48:06 +0200

    Trying to setup Travis and PDK.

--- .setup_pdk.sh
+++ .setup_pdk.sh
@@ -1,7 +1,8 @@
 # Install skywater-pdk (https://github.com/google/skywater-pdk)
 if [ ! -f $INSTALL_DIR/skywater-pdk ]; then
   mkdir -p $INSTALL_DIR
-  git clone git@github.com:google/skywater-pdk.git
+  #git clone git@github.com:google/skywater-pdk.git
+  git clone https://github.com/google/skywater-pdk
   cd skywater-pdk
   git checkout 4e5e318e0cc578090e1ae7d6f2cb1ec99f363120
   git submodule update --init libraries/sky130_fd_sc_hd/latest
