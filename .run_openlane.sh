230db364dfe6e10c65392b79eca66bfdb0f253eb
baywatcher <>
2020-08-27 23:09:05 +0200

    Trying out Travis with Openlane and SkyWater.

--- .run_openlane.sh
+++ .run_openlane.sh
@@ -10,8 +10,10 @@ ls $(pwd)/openlane
 ls $INSTALL_DIR
 ls $INSTALL_DIR/sky130A
 ls $INSTALL_DIR/sky130A/libs.ref
+ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd
+ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd/lef
 pwd
-docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "ls /openLANE_flow; ls openlane; openlane/flow.tcl -design spm"
+docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "ls /openLANE_flow; ls openlane; ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd/lef; openlane/flow.tcl -design spm"
 
 # Run flow for design SPM as test
 #source flow.tcl -design spm
