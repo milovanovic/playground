dc3d7243fd76e87958d89c00489700f588d0f274
baywatcher <>
2020-08-28 21:05:36 +0200

    Travis, Openlane, PDKs

--- .run_openlane.sh
+++ .run_openlane.sh
@@ -4,15 +4,15 @@ set -ev
 # Setup docker container
 docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2
 #docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 "flow.tcl -design spm"
-ls $PDK_ROOT
-ls $TRAVIS_BUILD_DIR/openlane
-ls $(pwd)/openlane
-ls $INSTALL_DIR
+##ls $PDK_ROOT
+##ls $TRAVIS_BUILD_DIR/openlane
+##ls $(pwd)/openlane
+##ls $INSTALL_DIR
 #ls $INSTALL_DIR/sky130A
 #ls $INSTALL_DIR/sky130A/libs.ref
 #ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd
 #ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd/lef
-pwd
+#pwd
 #docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "ls openlane; ls /openLANE_flow; openlane/flow.tcl -design spm"
 # ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd/lef;
 # Run flow for design SPM as test
@@ -20,5 +20,5 @@ pwd
 #exit
 
 # Copy GDS file to transfer.sh
-gds_file="designs/spm/runs/*/results/magic/spm.gds"
+gds_file="designs/spm/runs/2*/results/magic/spm.gds"
 curl -n -T $gds_file https://transfer.sh
