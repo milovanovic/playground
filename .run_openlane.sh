47a424c849d4b7310d3578e97401b368363b3375
baywatcher <>
2020-08-29 17:39:49 +0200

    X4

--- .run_openlane.sh
+++ .run_openlane.sh
@@ -2,19 +2,19 @@
 set -ev
 
 # Setup docker container
-#docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2
+docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2
 #docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 "flow.tcl -design spm"
-ls $PDK_ROOT
+#ls $PDK_ROOT
 ##ls $TRAVIS_BUILD_DIR/openlane
 ##ls $(pwd)/openlane
-ls $INSTALL_DIR
-ls $PDK_ROOT/sky130A
-ls $PDK_ROOT/sky130A/libs.ref
-ls $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd
-ls $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/lef
-pwd
-find -name '*sky13A*sky130_fd_sc_hd*.lef'
-docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "ls; find -name '*sky13A*sky130_fd_sc_hd*.lef'; openlane/flow.tcl -design spm"
+#ls $INSTALL_DIR
+#ls $PDK_ROOT/sky130A
+#ls $PDK_ROOT/sky130A/libs.ref
+#ls $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd
+#ls $PDK_ROOT/sky130A/libs.ref/sky130_fd_sc_hd/lef
+#pwd
+#find -name '*sky13A*sky130_fd_sc_hd*.lef'
+#docker run -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc2 /bin/sh -c "ls; find -name '*sky13A*sky130_fd_sc_hd*.lef'; openlane/flow.tcl -design spm"
 # ls $INSTALL_DIR/sky130A/libs.ref/sky130_fd_sc_hd/lef;
 # ls install: ls skywater-pdk; ls openlane; ls /openLANE_flow
 # Run flow for design SPM as test
